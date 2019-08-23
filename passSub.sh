b2b1c409576817262665fa6406830aa0f840be2a
Ben Tannenwald <benjamin.tannenwald@cern.ch>
2019-08-22 20:46:52 -0500

    add 0PU TDR samples

--- passSub.sh
+++ passSub.sh
@@ -11,3 +11,7 @@ sleep 15
 python submitSampleToCondor.py --outputDir zmumu_MTDTDR_200PU_$1 --inputTXTfile MuonIsolationAnalyzer/data/filelists/zmumu_files_xrd_MTDTDR_200PU.txt --nFilesPerJob 25
 sleep 15
 python submitSampleToCondor.py --outputDir ttbar_MTDTDR_200PU_$1 --inputTXTfile MuonIsolationAnalyzer/data/filelists/ttbar_files_xrd_MTDTDR_200PU.txt --nFilesPerJob 25
+
+python submitSampleToCondor.py --outputDir zmumu_MTDTDR_0PU_$1 --inputTXTfile MuonIsolationAnalyzer/data/filelists/zmumu_files_xrd_MTDTDR_NoPU.txt --nFilesPerJob 25
+sleep 15
+python submitSampleToCondor.py --outputDir ttbar_MTDTDR_0PU_$1 --inputTXTfile MuonIsolationAnalyzer/data/filelists/ttbar_files_xrd_MTDTDR_NoPU.txt --nFilesPerJob 10
